6288463915e9e18373df7f4e1e7e016a5071f5e7
mehak javed <122620928+MehakJaved21@users.noreply.github.com>
2025-02-10 00:12:18 -0600

    Update category.h

--- category.h
+++ category.h
@@ -1,7 +1,6 @@
 #ifndef CATEGORY_H
 #define CATEGORY_H
 
-
 #include <string>
 #include <vector>
 #include <memory>
@@ -15,9 +14,14 @@ private:
 
 public:
     category(const std::string &name);
+
     void add_product(std::shared_ptr<product> product);
-    void remove_product(const std::shared_ptr<product>& product);
+    void remove_product(const std::string &product_name);  // Now removes by name
     void add_subcategory(std::shared_ptr<category> subcategory);
+
+    std::vector<std::shared_ptr<product>> get_products() const;  // New method
+    std::vector<std::shared_ptr<category>> get_subcategories() const;  // New method
 };
 
 #endif // CATEGORY_H
+
